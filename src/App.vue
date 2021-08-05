931dc84d2683d63197cc3dc4da32a492640dd36c
Rostyslav Liashenko <liashenkorostyslav@gmail.com>
2021-08-05 14:30:36 +0300

    new commit

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
             <router-link :to="{ name: 'login' }">Войти</router-link>
           </li>
           <li>
-            <router-link :to="{ name: 'register' }">Регистрация!1</router-link>
+            <router-link :to="{ name: 'register' }">Регистрация!</router-link>
           </li>
         </template>
       </ul>
